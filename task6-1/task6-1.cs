80fa47acf8e74c23f44855fd2058bf7352951c0f
Vladimir <valenkin@yandex.ru>
2023-02-02 23:29:31 +0500

    task6-1

---
+++ task6-1/task6-1.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Задача 1: показать двумерный массив размером m×n заполненный вещественными числами");
+Console.WriteLine();
+Console.WriteLine("Введите количество строк массива");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите левый предел диапазона элементов массива ");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите правый предел диапазона элементов массива ");
+int max = int.Parse(Console.ReadLine()!);
+
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round (new Random().Next(min,max) + new Random().NextDouble(),3);
+        }
+    }
+}
+
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+double[,] matr = new double[m, n];
+
+FillArray(matr);
+Console.WriteLine("Вот так выглядит массив: ");
+PrintArray(matr);
