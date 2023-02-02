18c0fb2556c165f56449a297624c8e7177d0a359
Vladimir <valenkin@yandex.ru>
2023-02-02 23:39:43 +0500

    task6-2

---
+++ task6-2/task 6-2.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Задача 2: задать двумерный массив следующим правилом: A(m,n) = m+n");
+Console.WriteLine();
+Console.WriteLine("Введите количество строк массива");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива");
+int n = int.Parse(Console.ReadLine()!);
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = i + j;
+    }
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matr = new double[m, n];
+Console.WriteLine();
+FillArray(matr);
+Console.WriteLine("Вот так выглядит массив: ");
+PrintArray(matr);
+
